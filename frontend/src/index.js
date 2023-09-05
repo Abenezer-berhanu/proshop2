c8dbb3d4b21a88d544a1808b6d1849cb50d7c063
Abenezer <abenuberhanu271@gmail.com>
2023-09-05 13:03:55 +0300

    product page finish

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import App from "./App";
+// import "bootstrap/dist/css/bootstrap.min.css"
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
 
-import reportWebVitals from './reportWebVitals';
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />}/>
+    </Route>
+  )
+)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
