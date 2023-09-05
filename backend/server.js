c2ff73a50243b4ee903bdb20037911690119e7fd
Abenezer <abenuberhanu271@gmail.com>
2023-09-05 23:57:37 +0300

    error handler add

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import products from "./products.js";
 import productRouter from './routes/productRoutes.js'
 import connectDB from "./config/DataBase.js";
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,9 @@ app.use(express.json());
 app.use('/api/products', productRouter)
 
 connectDB();
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`server listning to port ${port}`);
