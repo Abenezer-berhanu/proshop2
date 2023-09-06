b5dc39726e96557b08803c54fc2d6267781c81fb
Abenezer <abenuberhanu271@gmail.com>
2023-09-06 11:42:31 +0300

    add to cart reducer finish

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,11 +1,47 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems : {}}
+const addDecimal = (num) => {
+    return (Math.round(num * 100) / 100).toFixed()
+}
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {}
-})
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
 
-export default cartSlice.reducer
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      //calculate items price
+      state.itemsPrice = addDecimal(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      )
+      //calculate shipping price (if order is over 100$ then free, else $30 shipping)
+      state.deliveryPrice = addDecimal(state.itemsPrice > 100 ? 0 : 10)
+      //calculate tax price
+      state.taxPrice = addDecimal(Number((0.15 * state.itemsPrice).toFixed(2)))
+
+      //calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.deliveryPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2)
+      localStorage.setItem('cart', state)
+    },
+  },
+});
+
+export const {addToCart} = cartSlice.actions
+export default cartSlice.reducer;
