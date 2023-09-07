e30075ba55275c38438745cc13cab90d386b6259
Abenezer <abenuberhanu271@gmail.com>
2023-09-07 23:14:22 +0300

    user middlware add

---
+++ backend/controllers/userController.js
@@ -0,0 +1,79 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../model/userModel.js";
+import jwt from 'jsonwebtoken'
+
+const userSignin = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+    if(user && (await user.matchPassword(password))){
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET,{ expiresIn: '30d'})
+
+        //SET token as HTTPS only cookie
+        res.cookie('jwt', token, {
+            httpOnly:true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSites: 'strict',
+            maxAge: 30 * 24 * 60 * 60 * 1000 // 30days
+        })
+        res.json({
+            _id: user._id,
+            email: user.email,
+            name: user.name,
+            isAdmin: user.isAdmin,
+        })
+    }else{
+        res.status(401);
+        throw new Error('Invalid password or Email')
+    }
+});
+
+const userSignup = asyncHandler(async (req, res) => {
+  res.send("auth signup");
+});
+
+//private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("auth logout");
+});
+
+//private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("my profile");
+});
+
+//private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("user update profile");
+});
+
+//private
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("all users");
+});
+
+//private
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete user");
+});
+
+//private
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get user by id");
+});
+
+//private
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("user update profile");
+});
+
+export {
+  userSignin,
+  userSignup,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { protect, admin } from '../middleware/authMiddleware.js';
+import {
+  userSignin,
+  userSignup,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controllers/userController.js";
+
+const router = express.Router()
+
+router.route('/').get(protect, admin, getUsers).post(userSignup)
+router.post('/logout', logoutUser) // public
+router.post('/login', userSignin)// public
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)// private till login
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)//private except admin
+
+export default router
