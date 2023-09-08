15a3b8203bfcefaaed174fd94f77b28fc3d3e588
Abenezer <abenuberhanu271@gmail.com>
2023-09-09 00:28:54 +0300

    order route and controller add to backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/DataBase.js";
 import cookieParser from 'cookie-parser'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -12,9 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 app.use(cookieParser())
-
 app.use('/api/products', productRouter)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 connectDB();
 
